14bf69d7c997ae5b5a9b5580dd02522157b5d670
Garrett Blackmon <garrett@yellowcard.io>
2020-01-15 00:53:18 -0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'angular-todo';
+  name: string = "angular-todo";
+
+  constructor() {
+    this.changeName("john");
+  }
+
+  changeName(name: string) {
+    this.name = name;
+  }
 }
